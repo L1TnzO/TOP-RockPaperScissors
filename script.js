074b927bf24c6178f281de932bb0e433266b1742
L1TnzO <thenzo22@gmail.com>
2023-12-26 13:35:08 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice (){
+
+};
+
+function playRound (playerChoice, computerChoice) {
+
+};
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+function game () {
+    
+};
