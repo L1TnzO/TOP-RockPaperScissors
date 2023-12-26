f656fe2e1a21f3fe656642143670473d6d0a47ad
L1TnzO <thenzo22@gmail.com>
2023-12-26 19:02:35 -0300

    references buttons and containers
    adds event listener to the buttons container

--- script.js
+++ script.js
@@ -10,38 +10,58 @@ function playRound (playerChoice, computerChoice) {
         case 'rock-scissors':
         case 'scissors-paper':
         case 'paper-rock':
+            console.log('player');
             return 'player';
         case 'rock-paper':
         case 'scissors-rock':
         case 'paper-scissors':
+            console.log('computer');
             return 'computer';
         default:
+            console.log('draw');
             return 'draw';
+        
     }
 };
 
-const playerChoice = "rock";
-
-function game () {
-    let playerCounter = 0;
-    let computerCounter = 0;
-    let drawCounter = 0;
-    let result;
-    for (let i = 0; i<5; i++){
-        result = playRound (playerChoice, getComputerChoice());
-        if (result == 'player'){
-            playerCounter += 1;
-        }
-        else if (result == 'computer'){
-            computerCounter += 1;
-        }
-        else{
-            drawCounter += 1;
-        }
+let playerCounter = 0;
+let computerCounter = 0;
+let drawCounter = 0;
+function game (result) {
+    globalThis.playerCounter;
+    globalThis.computerCounter;
+    globalThis.drawCounter;
+    if(playerCounter === 5 || computerCounter === 5 || drawCounter === 5){console.log('el juego termino')}
+    if (result == 'player'){
+        playerCounter +=1;
     }
+    else if (result == 'computer'){
+        computerCounter +=1;
+    }
+    else{
+        drawCounter +=1;
+    }
+    
     if (playerCounter < computerCounter){return 'computer wins'}
     else if (playerCounter > computerCounter){return 'player wins'}
     else {return 'is draw'}
 };
 
-console.log(game());
+//references buttons
+const rockBtn = document.querySelector('[data-action="rock"]');
+const paperBtn = document.querySelector('[data-action="paper"]');
+const scissorsBtn = document.querySelector('[data-action="scissors"]');
+
+//references containers
+const btnContainer = document.querySelector('.button-container')
+const bodyContainer = document.querySelector('.body-container');
+
+btnContainer.addEventListener('click', (e)=>{
+    let target = e.target;
+    if (target.tagName == 'BUTTON'){
+        result = playRound (target.getAttribute('data-action'), getComputerChoice ());
+        game(result);
+        console.log(playerCounter)
+        
+    }
+})
